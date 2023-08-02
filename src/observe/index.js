39447cb06c04e0ddf86c3c274b31a8a90e4377ac
ll799268 <1010023095@qq.com>
2023-08-02 22:29:57 +0800

    feat observe

---
+++ src/observe/index.js
@@ -0,0 +1,39 @@
+
+const data = {
+  key1: 1,
+  key2: 2,
+}
+
+observe3();
+// function observe2() {
+//   for (const key in data) {
+//     let initVal = data[key];
+//     Object.defineProperty(data, key, {
+//       get() {
+//         console.log('获取', initVal);
+//         return initVal;
+//       },
+//       set(val) {
+//         console.log('设置', val);
+//         initVal = val;
+//       }
+//     })
+//   }
+// }
+
+function observe3() {
+  const newData = new Proxy(data, {
+    get(oTarget, key) {
+      console.log('获取', oTarget[key]);
+      return oTarget[key];
+    },
+    set(oTarget, key, val) {
+      console.log('设置', val);
+      oTarget[key] = val;
+    }
+  });
+
+  newData.key1
+  newData.key2 = 22
+  console.log(newData);
+}
